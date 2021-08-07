85fa1cf8746696b72e9932f77d7e510d1490cbce
JPT96 <joshuathomas96@msn.com>
2021-08-07 14:07:59 +0000

    created a func for board H&w with + and -

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
-shoot = input("Orders Commander?\n")
-print(shoot)
-xstr, ystr = shoot.split(",")
-x = int(xstr)
-y = (ystr)
+def board_h_w (board_width, board_height):
+    print("+" + "-" * board_width + "+")
+if __name__ == "__main__":
+    board_h_w(10,10)
+    shoot = input("Orders Commander?\n")
+    print(shoot)
+    xstr, ystr = shoot.split(",")
+    x = int(xstr)
+    y = (ystr)

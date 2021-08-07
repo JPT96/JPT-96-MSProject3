af1fa9640b090572c39c28db58ccc4e19ae1c80b
JPT96 <joshuathomas96@msn.com>
2021-08-07 14:22:09 +0000

    create arow empty listand a append for x,y inshots

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
-def board_h_w (board_width, board_height):
+def board_h_w (board_width, board_height, shots):
+    header_bottom = print("+" + "-" * board_width + "+")
     print("+" + "-" * board_width + "+")
+    for y in range (board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots:
+                row.append("X")
+        print("¦" + " " * board_width)
+    header_bottom = print("+" + "-" * board_width + "+")
 if __name__ == "__main__":
     board_h_w(10,10)
     shoot = input("Orders Commander?\n")
@@ -7,3 +15,4 @@ if __name__ == "__main__":
     xstr, ystr = shoot.split(",")
     x = int(xstr)
     y = (ystr)
+    

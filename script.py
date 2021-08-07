7dbafcd84c8b35a6ddbcc9ead3beda2946c28cd3
JPT96 <joshuathomas96@msn.com>
2021-08-07 15:35:16 +0000

    add battle ships to board with for loops

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ class battleship(object):
         self.body = body
 b = battleship.build((1,1),3,"S")        
 def board_h_w (board_width, board_height, shots):
-    header_bottom = ("+" + "-" * board_width + "+")
+    header_bottom = "+" + "-" * board_width + "+"
     print(header_bottom)
     shots_set = set(shots)
     for y in range (board_height):
@@ -33,13 +33,43 @@ def board_h_w (board_width, board_height, shots):
         print ("|" + "".join(row) + "|")        
         print("|" + " " * board_width)
     print(header_bottom)
+def render_battleships(board_width,board_height, battleships):
+    header_bottom = "+" + "-" * board_width + "+"
+    print(header_bottom)
+    #build empty board
+    board = []
+    for x in range(board_width):
+        row = []
+        for y in range (board_height):
+            row.append(None)
+        board.append(row)
+        #add the battleships
+    for b in battleships:
+        for x, y in b.body:  
+            board [x] [y] = "O"
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            row.append(board[x][y] or " ")
+        print("|"+"".join(row) + "|")    
+
+    print(board)
+    print(header_bottom)    
+
+
+
+
+
 if __name__ == "__main__":
     battleships = [
         battleship.build((3,4),2,"N"),
         battleship.build((4,8),4,"N"),
         battleship.build((1,1),5,"E"),
         battleship.build((1,1),4,"W")
-    ]     
+    ]    
+    for b in battleships:
+        print(b.body)
+    render_battleships(10,10, battleships) 
 
 
 

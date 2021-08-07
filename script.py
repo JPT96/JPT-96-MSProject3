443b2a7140f680c769833941fd6994ad76948e99
JPT96 <joshuathomas96@msn.com>
2021-08-07 17:12:03 +0000

    create a class for theboard with BSheight widshots

--- script.py
+++ script.py
@@ -2,5 +2,12 @@ import time
 import random
 import copy
 
+class gaming_board(object):
+    def __init__(self,battleships,width,height):
+        self.battleships = battleships
+        self.width = width
+        self.height = height
+        self.shots = []
+        
 
 

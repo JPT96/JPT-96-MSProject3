00c86c6de010a7beaf64c1f0553ca27a3ff154a1
JPT96 <joshuathomas96@msn.com>
2021-08-07 14:38:14 +0000

    create a battleship class and create a init forBS

--- script.py
+++ script.py
@@ -1,21 +1,30 @@
+class battleship(object):
+
+    def __init__(self,body):
+        self.body = body
 def board_h_w (board_width, board_height, shots):
     header_bottom = print("+" + "-" * board_width + "+")
     print("+" + "-" * board_width + "+")
+    shots_set = set(shots)
     for y in range (board_height):
         row = []
         for x in range(board_width):
-            if (x,y) in shots:
+            if (x,y) in shots_set:
                 sh = "X"
-                else:
-                    sh = " "
+            else:
+                sh = " "
                 row.append(sh)
-        print("¦" + " " * board_width)
+        print ("|" + "".join(row) + "|")        
+        print("|" + " " * board_width)
     header_bottom = print("+" + "-" * board_width + "+")
 if __name__ == "__main__":
-    board_h_w(10,10)
-    shoot = input("Orders Commander?\n")
-    print(shoot)
-    xstr, ystr = shoot.split(",")
-    x = int(xstr)
-    y = (ystr)
-    
+    shots = []
+    while True:
+
+        shoot = input("Orders Commander?\n")
+        print(shoot)
+        xstr, ystr = shoot.split(",")
+        x = int(xstr)
+        y = (ystr)
+        shots.append((x,y))
+        board_h_w(10,10,shots)

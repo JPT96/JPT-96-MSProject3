70ce366ef73f8fe1ee7aac1507760e7d54c52118
JPT96 <joshuathomas96@msn.com>
2021-08-08 14:06:07 +0000

    add a list with 0 in the rand coll

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ print(grid_layout)
 def rand_row(board):
     return randint (0,len(board) -1)
 def rand_coll(board):
-    return randint (0,len(board) -1) 
+    return randint (0,len(board[0]) -1) 
 
 boat_row = rand_row
 boat_coll = rand_coll

9803bb7c4be55afa09c6556e47452325a4a17616
JPT96 <joshuathomas96@msn.com>
2021-08-08 13:51:42 +0000

    create a empty list and func for five rows

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
 import time
 import random
 
+grid = []
 
+for x in range(0,5):
+    grid.append(["O"]*5)
+print (grid)
+def grid_layout (grid):
+    for x in grid:
+        print (x)
+print(grid_layout)        

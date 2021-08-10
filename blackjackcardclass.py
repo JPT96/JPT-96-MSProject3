2000e23c5ce43c5a40b1d2ec2702ba05a78043c4
JPT96 <joshuathomas96@msn.com>
2021-08-10 18:46:02 +0000

    create  card func and with | - create card border

--- blackjackcardclass.py
+++ blackjackcardclass.py
@@ -3,4 +3,12 @@ class card:
         self.price = value
         self.value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"][value -1]
         self.suit = "♥♦♠♣" [suit]
-        
+    
+    def card (self):
+        print('┌───────┐')
+        print(f'| {self.value:<2}    |')
+        print('|       |')
+        print(f'|   {self.suit}   |')
+        print('|       |')
+        print(f'|    {self.value:>2} |')
+        print('└───────┘') 

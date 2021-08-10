6b13779731c6d13323189a2d008d5c262201d4dd
JPT96 <joshuathomas96@msn.com>
2021-08-10 18:43:06 +0000

    assign card values to self.values apply suits

--- blackjackcardclass.py
+++ blackjackcardclass.py
@@ -2,5 +2,5 @@ class card:
     def __init__(self,value,suit):
         self.price = value
         self.value = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-        
+        self.suit = "♥♦♠♣" [suit]
         
